b83cb87d8ed8beb13bd01d5902e36494639cff5d
Bradley van Ree <38775942+Brad-eki@users.noreply.github.com>
2020-05-03 15:55:17 +1000

    Update setup.py
    Co-Authored-By: Bradley van Ree <bradbase@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xlfunctions-bradbase",
+    name="xlfunctions",
     version="0.0.1b",
     author="Bradley van Ree",
     author_email="brads@bradbase.net",
