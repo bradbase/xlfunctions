d03d2888f85b7967b0356716b46e075d5ec39d01
Bradley van Ree <38775942+Brad-eki@users.noreply.github.com>
2020-05-11 15:49:37 +1000

    Bugfix function ABS

--- xlfunctions/excel_lib.py
+++ xlfunctions/excel_lib.py
@@ -19,7 +19,7 @@ from pandas import DataFrame
 # in the FunctionNode code, so adding them here will have no effect.
 
 SUPPORTED_FUNCTIONS = {
-    "ABS":"xAbs.abs",
+    "ABS":"xAbs.xabs",
     "AVERAGE":"Average.average",
     "CHOOSE":"Choose.choose",
     "_XLFN.CONCAT":"Concat.concat",
