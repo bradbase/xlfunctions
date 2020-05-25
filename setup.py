782af50c9bab92f2945f7869b5ca55fca026c37a
Stephan Richter <stephan.richter@gmail.com>
2020-05-25 13:41:58 -0400

    Back to development: 0.1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ TESTS_REQUIRE = [
 
 setup(
     name="xlfunctions",
-    version='0.1.0',
+    version='0.1.1.dev0',
     author="Bradley van Ree",
     author_email="brads@bradbase.net",
     description=(
